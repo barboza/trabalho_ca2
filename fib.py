86a4812a4b338351bf4dc609932469f1dfd0f84f
Rafael Barboza Cardoso <barboza.cardoso@gmail.com>
2012-06-21 10:53:47 -0300

    ex 1 e 2 completos

--- fib.py
+++ fib.py
@@ -4,13 +4,13 @@ import threading
 memoria = []
 # Vetor de eventos
 events = []
-# Semáforo para leitura da memória
+# Semaforo para leitura da memaria
 semMemoria = threading.BoundedSemaphore(1)
 
 
 class fib ( threading.Thread ):
 
-	# Sobrescreve o método __init__, para possibilitar a passagem de parâmetros para a thread
+	# Sobrescreve o metodo __init__, para possibilitar a passagem de parametros para a thread
 	def __init__ ( self, number):
 		self.number = number
 		threading.Thread.__init__ ( self )
@@ -21,25 +21,27 @@ class fib ( threading.Thread ):
 		semMemoria.release()
 		
 		if(self.number > 1):
-			fib(self.number-1).start() # inicia a thread to número anterior
-			events[self.number-1].wait() # Espera até que a thread termine de calcular
+			fib(self.number-1).start() # inicia a thread to numero anterior
+			events[self.number-1].wait() # Espera ate que a thread termine de calcular
 			semMemoria.acquire()
-			memoria.append(memoria[self.number-1]+memoria[self.number-2]) # Calcula o fibonacci baseado nos valores em memória
+			memoria.append(memoria[self.number-1]+memoria[self.number-2]) # Calcula o fibonacci baseado nos valores em memoria
 			semMemoria.release()
 		else:
 			if self.number == 1:
 				fib(self.number-1).start() # inicia o fibonacci de 0
 				events[self.number-1].wait() # Espera terminar
 			semMemoria.acquire()
-			memoria.append(self.number) # para 0 e 1 salva o próprio número na memória
+			memoria.append(self.number) # para 0 e 1 salva o proprio numero na memoria
 			semMemoria.release()
 
-		events[self.number].set() # dispara o evento, dizendo que a thread está concluida
+		events[self.number].set() # dispara o evento, dizendo que a thread esta concluida
 		
+number = 10 # posicao do array do qual se deseja saber o fibonacci
 
-for x in range(11):
+for x in range(number+1): # [0] ate [10]
 	events.append(threading.Event()) # inicializa o vetor de eventos
 
-fib(10).start() # inicia o algoritmo
-events[10].wait() # espera até que a thread inicial esteja completa
+fib(number).start() # inicia o algoritmo
+events[number].wait() # espera ate que a thread inicial esteja completa
 print memoria # exibe o vetor com os valores calculado
+print memoria[number] # Exibe o fibonacci na posição desejada
