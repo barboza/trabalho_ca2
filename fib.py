b3a23f626d447b8d33f61ecc269fb4f862c1dc09
Rafael Barboza Cardoso <barboza.cardoso@gmail.com>
2012-06-22 09:51:44 -0300

    adicionada segunda thread no fibonacci

--- fib.py
+++ fib.py
@@ -17,31 +17,31 @@ class fib ( threading.Thread ):
 
 	def run ( self ):
 		semMemoria.acquire()
-		lenMem = len(memoria)
+		memAtual = memoria[self.number]
 		semMemoria.release()
-		
-		if(self.number > 1):
-			fib(self.number-1).start() # inicia a thread to numero anterior
-			events[self.number-1].wait() # Espera ate que a thread termine de calcular
-			semMemoria.acquire()
-			memoria.append(memoria[self.number-1]+memoria[self.number-2]) # Calcula o fibonacci baseado nos valores em memoria
-			semMemoria.release()
-		else:
-			if self.number == 1:
-				fib(self.number-1).start() # inicia o fibonacci de 0
-				events[self.number-1].wait() # Espera terminar
-			semMemoria.acquire()
-			memoria.append(self.number) # para 0 e 1 salva o proprio numero na memoria
-			semMemoria.release()
+		if memAtual < 0:
+			if(self.number > 1):
+				t1 = fib(self.number-1) # inicia a thread do numero anterior
+				t2 = fib(self.number-2) # inicia a thread de dois numeros antes
+				t2.start() # inicia as threads
+				t1.start()
+				t1.join() # espera ate que o numero anterior termine de calcular
+				semMemoria.acquire()
+				memoria[self.number] = memoria[self.number-1]+memoria[self.number-2] # Calcula o fibonacci baseado nos valores em memoria
+				semMemoria.release()
+			else:
+				semMemoria.acquire()
+				memoria[self.number] = self.number # para 0 e 1 salva o proprio numero na memoria
+				semMemoria.release()
 
-		events[self.number].set() # dispara o evento, dizendo que a thread esta concluida
-		
+
 number = 10 # posicao do array do qual se deseja saber o fibonacci
 
-for x in range(number+1): # [0] ate [10]
-	events.append(threading.Event()) # inicializa o vetor de eventos
+for x in range(number+1): # inicializa a memoria
+	memoria.append(-1)
 
-fib(number).start() # inicia o algoritmo
-events[number].wait() # espera ate que a thread inicial esteja completa
+thread = fib(number)
+thread.start() # inicia o algoritmo
+thread.join() # espera o processo ser concluido
 print memoria # exibe o vetor com os valores calculado
-print memoria[number] # Exibe o fibonacci na posição desejada
+print memoria[number] # Exibe o fibonacci na posicao desejada
